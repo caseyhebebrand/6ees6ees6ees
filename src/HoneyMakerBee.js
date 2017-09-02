802dcd9c9c1a474329177797bdd0c3ee8ac06dc4
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-09-01 22:21:23 -0700

    6ees6ees6ees ES6 implementation

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeypot = 0;
+  }
+
+  makeHoney() {
+    this.honeypot++;
+  }
+
+  giveHoney() {
+    this.honeypot--;
+  }
 };
